7a2d7589b36cda410d6692a31784994059b7ed7c
Justus Lind <deamonhunterextra@outlook.com>
2025-06-11 20:09:27 +1000

    Various Dialogue fixes. Hopefully they all work.

--- ArchipelagoEverhood/Patches/ChestPatches.cs
+++ ArchipelagoEverhood/Patches/ChestPatches.cs
@@ -4,6 +4,7 @@ using Fungus;
 using HarmonyLib;
 using MelonLoader;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ArchipelagoEverhood.Patches
 {
@@ -375,22 +376,43 @@ namespace ArchipelagoEverhood.Patches
     {
         private static string? _overrideTextValue;
 
-        private static void Prefix(Say __instance, ref string ___overridingName)
+        private static HashSet<string> _bannedSays = new()
         {
-            if (!Globals.SessionHandler.LoggedIn)
+            "Companion Dialogue box"
+        };
+
+        private static void Prefix(Say __instance, ref string ___overridingName, ref Character ___character,
+            ref Characters ___characterByEnum, ref CharacterPortrait ___characterPortrait, ref TextType ___textType, ref bool ___showAlways,
+            ref bool ___extendPrevious, ref bool ___waitForClick, ref Color ___overridingNameColor, ref Color ___overridingDialogueColor,
+            ref bool ___disableInstantComplete, out bool __state)
+        {
+            __state = false;
+            if (!Globals.SessionHandler.LoggedIn || _overrideTextValue == null)
                 return;
 
-            Globals.Logging.Log("Say", "Triggered");
-
-            if (_overrideTextValue == null)
+            Globals.Logging.Log("Say", $"Triggered Override: {_overrideTextValue}");
+            if (_bannedSays.Contains(__instance.gameObject.name))
+            {
+                Globals.Logging.Log("Say", "Skipped due to being inventory.");
                 return;
+            }
 
-            Globals.Logging.Log("Say", $"Had Override Text: {_overrideTextValue}");
-
+            __state = true;
+            //___character = null;
+            ___characterByEnum = Characters.None;
+            ___characterPortrait = CharacterPortrait.None;
+            ___textType = TextType.Normal;
             ___overridingName = "Archipelago";
+            ___showAlways = true;
+            ___extendPrevious = false;
+            ___waitForClick = true;
+            ___overridingNameColor = Color.white;
+            ___overridingDialogueColor = Color.white;
+            ___disableInstantComplete = true;
             __instance.SetStoryText(_overrideTextValue);
             _overrideTextValue = null;
             SayGetStringIdPatch.Override = true;
+            WriterProcessTokens.Override = true;
         }
 
         public static void SetOverrideText(string text)
@@ -406,15 +428,20 @@ namespace ArchipelagoEverhood.Patches
             var topDown = GameObject.FindFirstObjectByType<Main_TopdownRoot>(FindObjectsInactive.Include);
             var dialog = topDown.GetSayDialogue(DialogueBoxType.Topdown_NoPortrait);
             dialog.SetActive(true);
+            dialog.SetCharacter(null);
+            dialog.SetCharacterImage(null);
+            dialog.SetJustCharacterName("Archipelago");
+            dialog.SetCharacterNameColor(Color.white);
+            dialog.SetCharacterDialogueColor(Color.white);
             dialog.Writer.SetDisableInstanteComplete(true);
-            dialog.NameText.text = "Archipelago";
             SayDialog.ActiveSayDialog = dialog;
-
+            WriterProcessTokens.Override = true;
             dialog.Say(text, true, true, true, false, false, null, () =>
             {
                 try
                 {
                     Globals.Logging.Warning("ForceShowDialogue", "Unlocking Movement");
+                    dialog.Writer.SetDisableInstanteComplete(false);
                     if (unlockMovement)
                         topDown.Player.SetTopDownPlayerMovementState(true);
 
@@ -455,6 +482,36 @@ namespace ArchipelagoEverhood.Patches
             Override = false;
             __result = "-9999";
             return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Writer), "ProcessTokens")]
+    public static class WriterProcessTokens
+    {
+        public static bool Override;
+        private static bool _originalIgnore;
+        private static float _originalSpeed;
+
+        private static void Prefix(ref bool stopAudio, ref bool ___ignoreSettingsTextSpeed, ref float ___writingSpeed)
+        {
+            if (!Globals.SessionHandler.LoggedIn || !Override)
+                return;
+
+            stopAudio = true;
+            _originalIgnore = ___ignoreSettingsTextSpeed;
+            ___ignoreSettingsTextSpeed = true;
+            _originalSpeed = ___writingSpeed;
+            ___writingSpeed = 999999f;
+        }
+
+        private static void PostFix(ref bool stopAudio, ref bool ___ignoreSettingsTextSpeed, ref float ___writingSpeed)
+        {
+            if (!Globals.SessionHandler.LoggedIn || !Override)
+                return;
+
+            Override = false;
+            ___ignoreSettingsTextSpeed = _originalIgnore;
+            ___writingSpeed = _originalSpeed;
         }
     }
 }
