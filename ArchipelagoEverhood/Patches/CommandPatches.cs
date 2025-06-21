39da6d0ccef17710da9e6ecd1b9d66ebb28e426a
Justus Lind <deamonhunterextra@outlook.com>
2025-06-21 15:29:32 +1000

    Oops. This is part of the chest reward fix.

---
+++ ArchipelagoEverhood/Patches/CommandPatches.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ArchipelagoEverhood.Data;
+using Fungus;
+using HarmonyLib;
+
+namespace ArchipelagoEverhood.Patches
+{
+    [HarmonyPatch(typeof(BooleanVariable), "Apply")]
+    public static class BooleanVariableHookPatch
+    {
+        private static void Postfix(BooleanVariable __instance, SetOperator setOperator, bool value)
+        {
+            if (!Globals.SessionHandler.LoggedIn)
+                return;
+            
+            try
+            {
+                if (setOperator != SetOperator.Assign)
+                    return;
+                var data = Globals.EverhoodChests.OnBoolVariableSet(__instance.Key, value);
+                if (data == null)
+                    return;
+                
+                Globals.Logging.Log("BooleanVariableHook", $"Got Variable: {__instance.Key}");
+                
+                var itemText = Globals.EverhoodChests.GetItemName(data);
+                if (data.RewardConditions.HasFlag(RewardConditions.ForceShowDialogue))
+                {
+                    if (data.RewardConditions.HasFlag(RewardConditions.ForceShowDialogue) && data.Shown)
+                        return;
+
+                    data.Shown = true;
+                    SayOnEnterPatch.ForceShowDialogue(itemText, null);
+                }
+                else
+                {
+                    SayOnEnterPatch.SetOverrideText(itemText);
+                }
+            }
+            catch (Exception e)
+            {
+                Globals.Logging.Error("BooleanVariableHook", e);
+            }
+        }
+    }
+}
