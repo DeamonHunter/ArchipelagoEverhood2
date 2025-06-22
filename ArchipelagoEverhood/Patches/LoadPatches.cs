72b98f2756eee375f3c36b4eb21768814c16fe55
Justus Lind <deamonhunterextra@outlook.com>
2025-06-22 16:43:55 +1000

    When first entering hometown, force the scene to be the start scene so cinematics play.

--- ArchipelagoEverhood/Patches/LoadPatches.cs
+++ ArchipelagoEverhood/Patches/LoadPatches.cs
@@ -1,15 +1,29 @@
-﻿using HarmonyLib;
+﻿
+using ArchipelagoEverhood.Util;
+using HarmonyLib;
+using UnityEngine;
 
 namespace ArchipelagoEverhood.Patches
 {
     [HarmonyPatch(typeof(SceneManagerRoot), nameof(SceneManagerRoot.LoadTopdownScene))]
     public static class LoadTopdownScenePatch
     {
-        public static void Prefix(int buildIndexToLoad)
+        public static void Prefix(ref int buildIndexToLoad, ref Vector3 spawnPosition)
         {
             if (!Globals.SessionHandler.LoggedIn)
                 return;
 
+            //Home Town doesn't work quite correct.
+            if (buildIndexToLoad == 38)
+            {
+                Globals.Logging.LogDebug("SceneManagerRoot", $"Setting Back Scene to: {buildIndexToLoad}");
+                if (!EverhoodHelpers.HasFlag("GL_HomeTownVisit"))
+                {
+                    spawnPosition = new Vector3(-21.3f, -58.5f, 0);
+                    buildIndexToLoad = 39;
+                }
+            }
+            
             Globals.CurrentTopdownLevel = buildIndexToLoad;
             Globals.Logging.LogDebug("SceneManagerRoot", $"Setting Back Scene to: {buildIndexToLoad}");
         }
